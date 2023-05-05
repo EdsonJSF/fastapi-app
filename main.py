ca8949329037813e8b3ba151b4888e810da7450f
Edsonjsf <edsonjsf1990@gmail.com>
2023-05-05 09:55:33 -0400

    Documentación del Swagger y de la App

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from fastapi import FastAPI
 # Crea la aplicación (una instancia de FastAPI)
 app = FastAPI()
 
+# Documentación del Swagger y de la App
+app.title = "FastAPI App"
+app.version = "0.0.1"
+
 # endpoint
-@app.get('/')
+@app.get('/', tags=['Home'])
 def message():
     return 'Hola mundo'
