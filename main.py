a74e0c17a50d412cedc70a6b4161da8e23cb41b0
Edsonjsf <edsonjsf1990@gmail.com>
2023-05-05 10:16:16 -0400

    Uso del metodo GET

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 # Crea la aplicación (una instancia de FastAPI)
 app = FastAPI()
@@ -7,7 +8,23 @@ app = FastAPI()
 app.title = "FastAPI App"
 app.version = "0.0.1"
 
-# endpoint
+# Endpoints
 @app.get('/', tags=['Home'])
 def message():
-    return 'Hola mundo'
+    return HTMLResponse("""
+      <h1>Hola mundo<h1/>
+    """)
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    movies = [
+        {
+            'id': 1,
+            'title': 'Avatar',
+            'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+            'year': '2009',
+            'rating': 7.8,
+            'category': 'Acción'
+        }
+    ]
+    return movies
