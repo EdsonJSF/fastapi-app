20d4e25eb25973ee0ac9b461c005111acc6dc1eb
Edsonjsf <edsonjsf1990@gmail.com>
2023-05-05 11:08:45 -0400

    Uso de parámetros de ruta

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
+from data.main import movies
+
 # Crea la aplicación (una instancia de FastAPI)
 app = FastAPI()
 
 # Documentación del Swagger y de la App
 app.title = "FastAPI App"
 app.version = "0.0.1"
+
 
 # Endpoints
 @app.get('/', tags=['Home'])
@@ -15,16 +18,13 @@ def message():
       <h1>Hola mundo<h1/>
     """)
 
+
 @app.get('/movies', tags=['movies'])
 def get_movies():
-    movies = [
-        {
-            'id': 1,
-            'title': 'Avatar',
-            'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-            'year': '2009',
-            'rating': 7.8,
-            'category': 'Acción'
-        }
-    ]
     return movies
+
+
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie_by_id(id: int):
+    movie = list(filter(lambda movie : movie['id'] == id, movies))
+    return movie if len(movie) > 0 else []
