4aea23b45ced3169d6132706ad1a1f7acaa3b332
Edsonjsf <edsonjsf1990@gmail.com>
2023-05-11 09:50:27 -0400

    Implementacion de dotenv

--- jwt_manager.py
+++ jwt_manager.py
@@ -1,5 +1,11 @@
+from dotenv import load_dotenv
 from jwt import encode
+import os
+
+load_dotenv()
+
+jwt_key = os.getenv("JWT_KEY")
 
 
 def create_token(data: dict):
-    token: str = encode(payLoad=data, key="my_secret_key", algorithm="HS256")
+    token: str = encode(payLoad=data, key=jwt_key, algorithm="HS256")
